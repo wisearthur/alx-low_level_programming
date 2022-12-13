a83926746200b0bf87045b15539a09e1c805b7a9
wisearthur <wise20111@outlook.com>
2022-12-12 23:26:09 -0800

    tast

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
+/*
+ * File: 4-puts.c
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
-
-
-int main (void)
+int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+	        return (0);
 }
