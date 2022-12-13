071d13822887cb0f3a3ee54b0388d73bc0b32f58
wisearthur <wise20111@outlook.com>
2022-12-13 01:00:51 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of a int: %lu byte(s)\n", sizeof(int));
@@ -14,4 +14,4 @@ int main (void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-} 
+}
