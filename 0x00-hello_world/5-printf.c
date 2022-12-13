872b8b2f9c5849938b6431b0069c2373af4614c7
wisearthur <wise20111@outlook.com>
2022-12-13 00:50:55 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,8 @@
 int main(void)
 
 {
-	printf(" with proper grammar, but the outcome is \n a piece of art,");
+	printf(" with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 
 }
