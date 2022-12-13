d1f9c377d672c662e799bf59d1b069b05aca1f98
wisearthur <wise20111@outlook.com>
2022-12-12 21:49:53 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- *  *main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 int main (void)
 
 {
-       	printf("\" Programming is like building a multilingual puzzle");
+       	printf("\" with proper grammar, but the outcome is a piece of art,");
 	return (0);
 
 }
