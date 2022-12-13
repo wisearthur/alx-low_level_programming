c92c01e0279093aae955bba1a17149cc9f9649ea
wisearthur <wise20111@outlook.com>
2022-12-13 02:18:15 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 
 int main(void)
@@ -14,5 +14,3 @@ int main(void)
 	
 	return (0);
 }
-
-
