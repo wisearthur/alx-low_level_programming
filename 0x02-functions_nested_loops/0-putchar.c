24fedff7c914ca9ec2ea1d5b010c1cf24772adf9
wisearthur <wisearthur1@gmail.com>
2023-02-07 23:57:42 -0800

    taske 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+/** 
+ * main - print putchar 
+ * Return: always 0
+ */
+
+# include "main.h"
+
+int main(void)
+
+{
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+
+ return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a Function, nested loops C Programming
